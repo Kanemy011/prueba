ea22c2a9aa5b85c84301343da4c06186c7de7642
Kanemy011 <gmmagarcal@gmail.com>
2024-12-19 10:08:47 +0100

    Add files via upload

---
+++ js/about.a1f78c85.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksubida_guithub"]=self["webpackChunksubida_guithub"]||[]).push([[594],{417:function(u,t,n){n.r(t),n.d(t,{default:function(){return r}});var s=function(){var u=this;u._self._c;return u._m(0)},i=[function(){var u=this,t=u._self._c;return t("div",{staticClass:"about"},[t("h1",[u._v("This is an about page")])])}],a=n(656),e={},l=(0,a.A)(e,s,i,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.a1f78c85.js.map
